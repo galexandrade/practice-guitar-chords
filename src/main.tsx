5272b2b44480f223284fce7f9f5a54ac4f9f57d7
Alex Andrade <alex.andrade@7shifts.com>
2024-02-04 16:20:51 -0600

    Making a PWA

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,19 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </SousChefProvider>
     </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log(
+                    'Service Worker registered with scope:',
+                    registration.scope
+                );
+            })
+            .catch((error) => {
+                console.error('Service Worker registration failed:', error);
+            });
+    });
+}
